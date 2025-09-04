441cec175629688a6e04addf28c8cedbe91c1b6d
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-09-04 19:55:20 -0300

    atualizado vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/cabelereira/' : '/',
+    base: process.env.NODE_ENV === 'development'
+    ? '/' // sempre raiz no dev
+    : process.env.VERCEL
+    ? '/' // raiz no vercel
+    : '/cabelereira/', // github pages
   plugins: [react()],
 })
